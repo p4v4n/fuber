26cc2d34b119f2db324b96d221b363d7ab874944
p4v4n <pavan.barcelona@gmail.com>
2018-10-11 04:42:33 +0530

    Add wrap-params middleware to parse query strings

--- src/fuber/core.clj
+++ src/fuber/core.clj
@@ -1,6 +1,7 @@
 (ns fuber.core
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.params :refer [wrap-params]]
             [compojure.core :refer [defroutes GET]]
             [compojure.route :refer [not-found]]))
 
@@ -14,7 +15,10 @@
   (GET "/" [] hello)
   (not-found "Page not found."))
 
+(def app
+  (wrap-params  routes))
+
 (defn -main
   [port]
-  (jetty/run-jetty (wrap-reload #'routes)
+  (jetty/run-jetty (wrap-reload #'app)
                    {:port (Integer. port)}))
