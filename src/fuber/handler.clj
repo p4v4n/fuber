2c891a86b5261942e89b7f762316cbc6c32fee1d
p4v4n <pavan.barcelona@gmail.com>
2018-10-11 21:46:19 +0530

    Remove populate-initial-cab-data from handler

--- src/fuber/handler.clj
+++ src/fuber/handler.clj
@@ -2,11 +2,6 @@
   (:require [fuber.model :as model]
             [fuber.helpers :as helpers]))
 
-(defn populate-initial-cab-data!
-  "Add 10 cabs to populate list-of-cabs"
-  []
-  (reset! model/list-of-cabs (helpers/generate-random-cabs 10)))
-
 
 (defn get-all-cabs
   [req]
