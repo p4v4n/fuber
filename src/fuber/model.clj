f7697d47f2e51e48f50db705c83822911b4917f8
p4v4n <pavan.barcelona@gmail.com>
2018-10-11 21:29:20 +0530

    Modify current-rides to active-rides and change it to list.

--- src/fuber/model.clj
+++ src/fuber/model.clj
@@ -1,5 +1,9 @@
 (ns fuber.model)
+(ns fuber.model
+  (:require
+   [fuber.helpers :as helpers]))
 
 (def list-of-available-cabs (atom []))
 
-(def current-rides (atom {}))
+(def active-rides (atom '()))
+
