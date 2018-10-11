e87bdf895b81c162c1c67cd634dee6ac35a908a3
p4v4n <pavan.barcelona@gmail.com>
2018-10-11 10:05:05 +0530

    Add helpers.clj with example data

---
+++ src/fuber/helpers.clj
@@ -0,0 +1,14 @@
+(ns fuber.helpers)
+
+;;example cab info
+(def example-cab {:id "some-uuid"
+                  :location {:latitude 50
+                             :longitude 70}
+                  :is-pink true})
+
+
+;;example user info
+(def example-user {:user-id "some-uuid"
+                   :location {:latitude 40
+                              :longitude 65}
+                   :is-hipster false})
