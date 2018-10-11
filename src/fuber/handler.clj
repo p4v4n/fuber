9bd64c8413eceb41e33211c9420c69a90413eae4
p4v4n <pavan.barcelona@gmail.com>
2018-10-11 21:52:59 +0530

    Modify book-a-ride to return final result
           - handles user-already-riding and cabs-not-available cases
           - else return the booked-cab data

--- src/fuber/handler.clj
+++ src/fuber/handler.clj
@@ -11,8 +11,19 @@
    :body  {:cabs @model/list-of-available-cabs}})
 
 (defn book-a-ride
+  "books a ride for user and returns the booked-cab"
   [req]
-  {})
+  (let [user (get-in req [:body :user])
+        nearest-cab (helpers/find-nearest-cab user
+                                              @model/list-of-available-cabs)]
+    (cond (model/is-user-riding? user)  "You can't book 2 cabs at the same time"
+          (nil? nearest-cab)  "Sorry.No cabs are currently available"
+          :else (do
+                  (model/remove-cab-from-available! nearest-cab)
+                  (model/add-cab-to-active-rides! user nearest-cab)
+                  {:status 200
+                   :headers {"Content-Type" "text/json"}
+                   :body {:cab nearest-cab}}))))
 
 (defn end-ride
   [req]
