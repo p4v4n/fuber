bdfc251cbb17ff14f7b1143039810a425efc844f
p4v4n <pavan.barcelona@gmail.com>
2018-10-11 11:02:06 +0530

    Add fn to populate list-of-cabs with 10 entries

--- src/fuber/handler.clj
+++ src/fuber/handler.clj
@@ -1,4 +1,12 @@
-(ns fuber.handler)
+(ns fuber.handler
+  (:require [fuber.model :as model]
+            [fuber.helpers :as helpers]))
+
+(defn populate-initial-cab-data!
+  "Add 10 cabs to populate list-of-cabs"
+  []
+  (reset! model/list-of-cabs (helpers/generate-random-cabs 10)))
+
 
 (defn get-all-cabs
   [req]
