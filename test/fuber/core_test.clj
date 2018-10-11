797e9651d71f758ca6c5a429c579fdbb4894d05e
p4v4n <pavan.barcelona@gmail.com>
2018-10-11 10:35:21 +0530

    Add tests for find-distance

--- test/fuber/core_test.clj
+++ test/fuber/core_test.clj
@@ -1,7 +1,19 @@
 (ns fuber.core-test
   (:require [clojure.test :refer :all]
-            [fuber.core :refer :all]))
+            [fuber.core :refer :all]
+            [fuber.helpers :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest distance-tests
+  (testing "testing distances between 2 locations"
+    (is (= 0.0 (find-distance  {:latitude 50
+                                :longitude 40}
+                               {:latitude 50
+                                :longitude 40})))
+    (is (= 5.0 (find-distance  {:latitude 10
+                                :longitude 20}
+                               {:latitude 13
+                                :longitude 16})))
+    (is (= 13.0 (find-distance  {:latitude 72
+                                 :longitude -90}
+                                {:latitude 84
+                                 :longitude -95})))))
