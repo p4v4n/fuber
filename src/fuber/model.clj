542180b38badb061efff56c0d91ce97e01df2eb5
p4v4n <pavan.barcelona@gmail.com>
2018-10-11 21:40:10 +0530

    Add example data for active-ride for clarity

--- src/fuber/model.clj
+++ src/fuber/model.clj
@@ -10,3 +10,15 @@
 (defn current-time-stamp
   []
   (quot (System/currentTimeMillis) 1000))
+
+;;Example Data for Active-Ride
+(def example-active-ride   {:ride-id {:cab {:cab-id "some-uuid"
+                                            :location {:latitude 50
+                                                       :longitude 70}
+                                            :is-pink true}
+                                      :user {:user-id "some-uuid"
+                                             :location {:latitude 50
+                                                        :longitude 70}
+                                             :is-hipster true}
+                                      :start-time "unix-timestamp"}})
+
